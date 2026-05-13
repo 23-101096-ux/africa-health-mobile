000c2c609c5a90741486a97407c3d74d16961238
nada yasser <nada@nadas-MacBook-Pro.local>
2026-05-13 17:45:35 +0300

    adjusted some things

--- healthmobile/src/pages/Registerpage.jsx
+++ healthmobile/src/pages/Registerpage.jsx
@@ -3,6 +3,9 @@ import { useNavigate } from "react-router-dom";
 import Button from "../components/Button";
 import "./Login.css"; 
 
+import google from "../assets/google.svg"; 
+import apple from "../assets/apple.svg"; 
+
 const RegisterPage = () => {
   const navigate = useNavigate();
 
@@ -48,8 +51,8 @@ const RegisterPage = () => {
         </div>
 
         <div className="social-box">
-          <button className="social-btn">G</button>
-          <button className="social-btn"></button>
+          <button className="social-btn"><img src={google} alt="" /></button>
+          <button className="social-btn"><img src={apple} alt="" /></button>
         </div>
       </div>
     </div>
